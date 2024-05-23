e8d13461db33572a405ca45d033b8ed61f843e20
jarlexdev <jarlexdev@gmail.com>
2024-05-22 18:44:02 -0600

    Guia 06 terminada

---
+++ GUIA06/query.sql
@@ -0,0 +1,7 @@
+SELECT v.idVenta, v.fechaHoraVenta ,c.nombresCliente, e.nombresEmpleado, m.nombreMedicamento, m.precioVenta
+FROM Ventas v
+INNER JOIN Clientes c ON v.idCliente = c.idCliente
+INNER JOIN Empleados e ON v.idEmpleado = e.idEmpleado
+INNER JOIN detalleVenta dv ON v.idVenta = dv.idVenta
+INNER JOIN Medicamentos m ON dv.idMedicamento = m.idMedicamento
+WHERE v.idVenta = 1;
