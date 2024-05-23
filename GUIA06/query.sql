56b43dbb1dbc0c135a2fb0fafa6870461d13817a
jarlexdev <jarlexdev@gmail.com>
2024-05-22 18:48:29 -0600

    Guia 06 terminada

--- GUIA06/query.sql
+++ GUIA06/query.sql
@@ -1,3 +1,4 @@
+-- CONSULTANDO UNA VENTA --
 SELECT v.idVenta, v.fechaHoraVenta ,c.nombresCliente, e.nombresEmpleado, m.nombreMedicamento, m.precioVenta
 FROM Ventas v
 INNER JOIN Clientes c ON v.idCliente = c.idCliente
