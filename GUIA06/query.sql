603ccaf371e5634b3472760a705147a4e0216a79
jarlexdev <jarlexdev@gmail.com>
2024-05-26 09:39:13 -0600

    Guia 06 terminada

--- GUIA06/query.sql
+++ GUIA06/query.sql
@@ -1,8 +1,39 @@
--- CONSULTANDO UNA VENTA --
-SELECT v.idVenta, v.fechaHoraVenta ,c.nombresCliente, e.nombresEmpleado, m.nombreMedicamento, m.precioVenta
-FROM Ventas v
-INNER JOIN Clientes c ON v.idCliente = c.idCliente
-INNER JOIN Empleados e ON v.idEmpleado = e.idEmpleado
-INNER JOIN detalleVenta dv ON v.idVenta = dv.idVenta
-INNER JOIN Medicamentos m ON dv.idMedicamento = m.idMedicamento
-WHERE v.idVenta = 1;
+-- CONSULTAS UTILIZANDO INNER JOIN --
+
+-- CONSULTANDO 01: 
+		-- Mostrar una lista de los medicamentos que se tienen a la venta, 
+		-- con los detalles del tipo de producto, presentación y proveedor (laboratorio)
+SELECT 
+    med.nombreMedicamento, 
+    cat.nombreCategoria,
+    mar.nombreMarca,
+    pre.nombrePresentacion,
+    lab.nombreLaboratorio
+FROM medicamentos med
+    INNER JOIN categorias cat ON med.idCategoria = cat.idCategoria
+    INNER JOIN marcas mar ON med.idMarca = mar.idMarca
+    INNER JOIN presentacionMedicamento pre ON med.idPresentacionMedicamento = pre.idPresentacionMedicamento
+    INNER JOIN detalleCompra dc ON med.idMedicamento = dc.idMedicamento
+    INNER JOIN compras com ON dc.idCompra = com.idCompra
+    INNER JOIN laboratorios lab ON com.idLaboratorio = lab.idLaboratorio;
+    
+-- CONSULTA 02: 
+	-- Mostrar una lista de las ventas realizadas, con los detalles del cliente que compró, 
+    -- el vendedor, y el monto de la venta incluyendo impuestos fiscales como el IVA y
+    -- crédito fiscal (si está registrando este último)
+SELECT
+  ven.fechaHoraVenta AS Fecha_Venta,
+  CONCAT(nc.nombresCliente, ' ', nc.apellidosCliente) AS Cliente,
+  CONCAT(emp.nombresEmpleado, ' ', emp.apellidosEmpleado) AS Empleado,
+  dv.cantidadVendida,
+  dv.precioUnitario,
+  (dv.cantidadVendida * dv.precioUnitario) AS SubTotal,
+  fv.ivaFacturaVenta AS IVA,
+  fv.creditoFiscalVenta AS Credito_Fiscal,
+  (dv.cantidadVendida * dv.precioUnitario) + fv.ivaFacturaVenta AS Total
+FROM ventas ven
+	INNER JOIN detalleVenta dv ON ven.idVenta = dv.idVenta
+	INNER JOIN medicamentos med ON dv.idMedicamento = med.idMedicamento
+	INNER JOIN clientes nc ON ven.idCliente = nc.idCliente
+	INNER JOIN empleados emp ON ven.idEmpleado = emp.idEmpleado
+	INNER JOIN facturaVenta fv ON dv.idFacturaVenta = fv.idFacturaVenta;
